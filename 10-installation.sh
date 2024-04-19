d2e14c5f98650c48634adf7eb3a2b10e7b84f6bd
daws-78 <swethasfdc68@gmail.com>
2024-04-20 04:38:55 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,9 +4,9 @@ if [ $USERID -ne 0]
 then
   echo "please run this script with root access."
 else
- echo :you are super user."
+ echo "you are super user."
  fi
 
 dnf install mysql -y
 
-echo "is script proceeding?'
+echo "is script proceeding?"

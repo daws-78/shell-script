a6d848b9567188350fa3f0041d186154c1a61eae
daws-78 <swethasfdc68@gmail.com>
2024-04-20 03:48:18 +0530

    hello script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,3 +7,5 @@ echo "Current working directory:$PWD"
 echo "Home directory of Current user: $HOME"
 echo "Hostname: $HOSTNAME"
 echo "process ID of the current shell script: $$"
+sleep 60 &
+echo "process ID of last background command: $!"

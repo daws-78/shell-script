933d2631834d8e5a69d117879e2d2e13a1b9eb3d
daws-78 <swethasfdc68@gmail.com>
2024-04-20 03:19:35 +0530

    hello script

---
+++ 08-special-vars.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory:$PWD"
+echo "Home directory of Current user: $HOME"

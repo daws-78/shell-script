c38bad66ad88d828cc416001ff94aae185b47adc
daws-78 <swethasfdc68@gmail.com>
2024-04-20 18:34:19 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -7,15 +7,13 @@ VALIDATE(){
     echo "what are you doing: $2"
 }
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
      exit 1 #manually exit if error comes.
 
 else
-
-    echo "you are super user."
-
+   echo "you are super user."
 fi
 
 dnf install mysql -y

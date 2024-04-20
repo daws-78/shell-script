c2eecb551b605e2f5f1d01cab81e0116723b1fbe
daws-78 <swethasfdc68@gmail.com>
2024-04-20 11:54:44 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -22,14 +22,17 @@ then
      echo "Installation of mysql...FAILURE"
     exit1
 else
-      echo "Installation of mysql...SUCCESS
+      echo "Installation of mysql...SUCCESS"
 fi
+
 dnf install git -y
+
 if [ $? -ne 0]
 then 
      echo "Installation of git...FAILURE"
      exit 1
 else
-   echo Installation of Git...SUCCESS
+   echo "Installation of Git...SUCCESS"
 fi
+
 echo "is script proceeding?"

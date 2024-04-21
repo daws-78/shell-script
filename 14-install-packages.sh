61e6d7f8f24ec51d4f141d3cd16f83c40fa637e3
daws-78 <swethasfdc68@gmail.com>
2024-04-22 00:31:00 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -2,7 +2,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$IMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -10,7 +10,7 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-       echo -e $2...$R FAILURE $N"
+       echo -e "$2...$R FAILURE $N"
        exit 1
      else
         echo -e "$2...$G SUCCESS $N"

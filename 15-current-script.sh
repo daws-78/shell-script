312c5cd432464554a753c77dfdcafc25b7fce83b
daws-78 <swethasfdc68@gmail.com>
2024-04-29 16:09:01 +0530

    functions

---
+++ 15-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "Before calling script,course: $COURSE"
+echo "Process ID of current script,course: $$"
+
+#./16-other-script.sh
+
+echo "After calling other script, course: $COURSE" 
---
+++ 16-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="Devops other script"
+
+echo "variable value from other script: $COURSE"
+echo "process ID of other script: $$"
